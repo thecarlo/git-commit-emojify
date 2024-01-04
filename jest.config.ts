5b6ee5d134c98a468c33e53e5d2f97541b9aaf09
Carlo van Wyk <5310264+thecarlo@users.noreply.github.com>
2024-01-04 11:32:11 +1100

    🔨 Added test coverage script

--- jest.config.ts
+++ jest.config.ts
@@ -12,5 +12,14 @@ const config: Config.InitialOptions = {
     '@maps/(.*)': '<rootDir>/src/maps/$1',
     '@configuration/(.*)': '<rootDir>/src/configuration/$1',
   },
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
 export default config;
